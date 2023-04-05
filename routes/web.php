0d37fbcd0438c94ec93c2e207d818691ce2e19fe
jacttis <julianacttis@hotmail.com>
2023-04-04 22:47:01 -0300

    Create view login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+
