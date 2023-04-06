dc1379c1c956263b0816c548a2f7823525b3fee2
LeanM <leandro.moran.11@gmail.com>
2023-04-06 11:30:46 -0300

    Revert "Update TeamSeeder.php"
    This reverts commit 72f9a045526853363c39cde720d7d8b6637bf7cd.

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -14,38 +14,36 @@ class TeamSeeder extends Seeder
      */
     public function run(): void
     {
-        $teams_stadiums_data = 
-            [   ["stadium_name" => "Wembley Stadium", "city" => "Londres", "team_name" => "Tottenham Hotspur F.C.", "capacity" => 90000],
-                ["stadium_name" => "Camp Nou", "city" => "Barcelona", "team_name" => "F.C. Barcelona", "capacity" => 99000],
-                ["stadium_name" => "Allianz Arena", "city" => "Múnich", "team_name" => "Bayern Múnich", "capacity" => 70000],
-                ["stadium_name" => "Old Trafford", "city" => "Manchester", "team_name" => "Manchester United F.C.", "capacity" => 74000],
-                ["stadium_name" => "San Siro", "city" => "Milán", "team_name" => "A.C. Milan", "capacity" => 80000],
-                ["stadium_name" => "Stade de France", "city" => "Saint-Denis", "team_name" => "Selección de Francia", "capacity" => 81000],
-                ["stadium_name" => "Estadio da Luz", "city" => "Lisboa", "team_name" => "S.L. Benfica", "capacity" => 65000],
-                ["stadium_name" => "Estadio do Dragao", "city" => "Oporto", "team_name" => "F.C. Porto", "capacity" => 50000],
-                ["stadium_name" => "Signal Iduna Park", "city" => "Dortmund", "team_name" => "Borussia Dortmund", "capacity" => 81000],
-                ["stadium_name" => "Estadio Santiago Bernabeu", "city" => "Madrid", "team_name" => "Real Madrid C.F.", "capacity" => 81000],
-                ["stadium_name" => "Stadio Olimpico", "city" => "Roma", "team_name" => "A.S. Roma", "capacity" => 72000],
-                ["stadium_name" => "Emirates Stadium", "city" => "Londres", "team_name" => "Arsenal F.C.", "capacity" => 60000],
-                ["stadium_name" => "Estadio Vicente Calderon", "city" => "Madrid", "team_name" => "Atlético de Madrid", "capacity" => 55000],
-                ["stadium_name" => "Estadio Benito Villamarin", "city" => "Sevilla", "team_name" => "Real Betis Balompié", "capacity" => 60000],
-                ["stadium_name" => "Anfield", "city" => "Liverpool", "team_name" => "Liverpool F.C.", "capacity" => 54000],
-                ["stadium_name" => "Stamford Bridge", "city" => "Londres", "team_name" => "Chelsea F.C.", "capacity" => 42000],
-                ["stadium_name" => "Parc des Princes", "city" => "París", "team_name" => "Paris Saint-Germain F.C.", "capacity" => 48000],
-                ["stadium_name" => "Juventus Stadium", "city" => "Turín", "team_name" => "Juventus F.C.", "capacity" => 41000],
-            ];
+        $teams_stadiums_data =  [
+            ["stadium_name" => "Wembley Stadium", "city" => "Londres", "team_name" => "Tottenham Hotspur F.C."],
+            ["stadium_name" => "Camp Nou", "city" => "Barcelona", "team_name" => "F.C. Barcelona"],
+            ["stadium_name" => "Allianz Arena", "city" => "Múnich", "team_name" => "Bayern Múnich"],
+            ["stadium_name" => "Old Trafford", "city" => "Manchester", "team_name" => "Manchester United F.C."],
+            ["stadium_name" => "San Siro", "city" => "Milán", "team_name" => "A.C. Milan e Inter de Milán"],
+            ["stadium_name" => "Stade de France", "city" => "Saint-Denis", "team_name" => "Selección de Francia"],
+            ["stadium_name" => "Estadio da Luz", "city" => "Lisboa", "team_name" => "S.L. Benfica"],
+            ["stadium_name" => "Estadio do Dragao", "city" => "Oporto", "team_name" => "F.C. Porto"],
+            ["stadium_name" => "Signal Iduna Park", "city" => "Dortmund", "team_name" => "Borussia Dortmund"],
+            ["stadium_name" => "Estadio Santiago Bernabeu", "city" => "Madrid", "team_name" => "Real Madrid C.F."],
+            ["stadium_name" => "Stadio Olimpico", "city" => "Roma", "team_name" => "A.S. Roma e S.S. Lazio"],
+            ["stadium_name" => "Emirates Stadium", "city" => "Londres", "team_name" => "Arsenal F.C."],
+            ["stadium_name" => "Estadio Vicente Calderon", "city" => "Madrid", "team_name" => "Atlético de Madrid"],
+            ["stadium_name" => "Estadio Benito Villamarin", "city" => "Sevilla", "team_name" => "Real Betis Balompié"],
+            ["stadium_name" => "Anfield", "city" => "Liverpool", "team_name" => "Liverpool F.C."],
+            ["stadium_name" => "Stamford Bridge", "city" => "Londres", "team_name" => "Chelsea F.C."],
+            ["stadium_name" => "Parc des Princes", "city" => "París", "team_name" => "Paris Saint-Germain F.C."],
+            ["stadium_name" => "Juventus Stadium", "city" => "Turín", "team_name" => "Juventus F.C."]
+    ];
 
         for ($i = 0; $i < count($teams_stadiums_data); $i++) {
             $stadium_data = $teams_stadiums_data[$i];
             $team_name = $stadium_data['team_name'] ?? '';
             $stadium_name = $stadium_data['stadium_name'] ?? '';
             $city = $stadium_data['city'] ?? '';
-            $capacity = $stadium_data['capacity'] ?? 0;
 
             $stadium = Stadium::factory()->state([
                 'stadium_name' => $stadium_name,
-                'located_on_city' => $city,
-                'capacity' => $capacity,
+                'located_on_city' => $city
             ])->create();
 
             $team = Team::factory()->state([
