ff1914df2ceed8d062c8c0cd1c5fe1dd69b8a198
jacttis <julianacttis@hotmail.com>
2023-04-13 20:46:33 -0300

    testing

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/public/',
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+            input: '/resources/js/app.js',
+        },
+    },
     plugins: [
         laravel({
             input: ['public/css/app.css', 'public/js/app.js'],
