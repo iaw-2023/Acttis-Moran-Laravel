73654b7faf10dda49342b7fa85a7eed831fa7952
jacttis <julianacttis@hotmail.com>
2023-04-14 17:18:16 -0300

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import commonjs from '@rollup/plugin-commonjs';
-import postcss from 'rollup-plugin-postcss';
 
 export default defineConfig({
+    base: '/',
     build: {
-        outDir: 'public',
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+            input: '/resources/js/app.js',
+        },
     },
     plugins: [
-        commonjs(), // Compila los archivos JS
-        postcss({ // Compila los archivos CSS
-            extract: true,
-            minimize: true,
+        laravel({
+            input: ['public/css/app.css', 'public/js/app.js'],
+            refresh: true,
         }),
     ],
 });
