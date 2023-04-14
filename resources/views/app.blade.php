75019eb93a94afb43e142fff0569e0ce5d6c8853
jacttis <julianacttis@hotmail.com>
2023-04-14 19:06:37 -0300

    testing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,7 +20,7 @@
     <!-- Styles -->
     <!-- CSS de Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ secure_asset('public/build/manifest.json') }}">
+    <link rel="stylesheet" href="{{ secure_asset('public/build/assets/app-525b4d55.css') }}">
 
 
     <!-- Agrega la referencia a jQuery -->
