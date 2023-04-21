cb938a6678503be4b500bc917edd5fed07a97eab
jacttis <julianacttis@hotmail.com>
2023-04-21 16:39:17 -0300

    Send email to reset password.

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+
+class ForgotPasswordController extends Controller
+{
+    use SendsPasswordResetEmails;
+
+    public function showLinkRequestForm()
+    {
+        return view('email');
+    }
+
+
+}
