860ddddfe350dda2f6f5b35e91a2bd0da6e534b3
LeanM <leandro.moran.11@gmail.com>
2023-05-13 11:30:24 -0300

    Revert "Update matchgames.blade.php"
    This reverts commit e4cc2b740b6985d5d89bb4160243f454a5a68f71.

--- resources/views/matchgames.blade.php
+++ resources/views/matchgames.blade.php
@@ -1,48 +1,39 @@
 @extends('home')
 @section('content')
 
-    <div class="view__container">
-        <span class="view__container__title">ABM Matchgames</span>
+    <div id="test">
+        <div>ABM Matchgames</div>
         <hr />
-        <div class="view__container__table-container">
+        <div class="table-responsive">
             <form method="GET" action="{{ route('matchgames.create') }}">
                     @csrf
                     @method('GET')
-<<<<<<< Updated upstream
                     <button class="btn btn-primary" type="submit" onclick="">Create Matchgame</button>
             </form>
             <table class="table">
                 <thead>
-=======
-                    <button class="function-button" type="submit" onclick="">Create Matchgame</button>
-            </form>  
-            <table class="view__container__table-container__table">
-                <thead class="view__container__table-container__table__head">
->>>>>>> Stashed changes
                 <tr>
                     <th scope="col">ID</th>
+                    <th scope="col">Created At</th>
+                    <th scope="col">Updated At</th>
                     <th scope="col">Home Team</th>
                     <th scope="col">Away Team</th>
                     <th scope="col">Stadium</th>
                     <th scope="col">Played on Date</th>
                     <th scope="col">Played on Time</th>
-                    <th scope="col">Created At</th>
-                    <th scope="col">Updated At</th>
                 </tr>
                 </thead>
-                <hr />
-                <tbody class="view__container__table-container__table__body">
+                <tbody>
                 @foreach ($matchgames as $matchgame)
-                    <tr class="view__container__table-container__table__item">
-                    
+                    <tr id="matchgame_id{{$matchgame->id}}">
                         <td>{{ $matchgame->id }}</td>
+                        <td>{{ $matchgame->created_at }}</td>
+                        <td>{{ $matchgame->updated_at }}</td>
                         <td>{{ $matchgame->teamsPlayingMatch[0]->team->team_name }}</td>
                         <td>{{ $matchgame->teamsPlayingMatch[1]->team->team_name }}</td>
                         <td>{{ $matchgame->stadium->stadium_name }}</td>
                         <td>{{ $matchgame->played_on_date }}</td>
                         <td>{{ $matchgame->played_on_time }}</td>
-                        <td>{{ $matchgame->created_at }}</td>
-                        <td>{{ $matchgame->updated_at }}</td>
                         <td>
                             @if($matchgame->deleted_at)
                                 <label class="text-danger">Deleted</label>
@@ -56,28 +47,18 @@
                             <form method="POST" action="{{ route('matchgames.delete', $matchgame->id) }}">
                                     @csrf
                                     @method('DELETE')
-<<<<<<< Updated upstream
                                     <button class="btn btn-primary" type="submit" onclick="return confirm('Are you sure you want to delete this Matchgame?')">Delete</button>
                             </form>
                             @endif
-=======
-                                    <button class="table-button" type="submit" onclick="return confirm('Are you sure you want to delete this Matchgame?')">Delete</button>
-                            </form>    
->>>>>>> Stashed changes
                         </td>
                         <td>
                             @if(!$matchgame->deleted_at)
                             <form method="GET" action="{{ route('matchgames.edit', $matchgame->id) }}">
                                     @csrf
                                     @method('GET')
-<<<<<<< Updated upstream
                                     <button class="btn btn-primary" type="submit" onclick="">Edit</button>
                             </form>
                             @endif
-=======
-                                    <button class="table-button" type="submit" onclick="">Edit</button>
-                            </form>    
->>>>>>> Stashed changes
                         </td>
 
                     </tr>
