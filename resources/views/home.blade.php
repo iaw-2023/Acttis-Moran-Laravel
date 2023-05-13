8019f7eec638347f99cfe7a824c0a2d5bfac2bee
LeanM <leandro.moran.11@gmail.com>
2023-05-13 11:29:10 -0300

    Styling the page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,25 +5,25 @@
         <form class="float-left" id="tickets-form" action="{{ route('tickets') }}" method="GET" style="display: none;">
             @csrf
         </form>
-        <button class="btn btn-primary nav__item__button" onclick="event.preventDefault(); document.getElementById('tickets-form').submit();">Tickets</button>
+        <button class="nav__item__button" onclick="event.preventDefault(); document.getElementById('tickets-form').submit();">Tickets</button>
     </li>
     <li class="nav-item">
         <form id="matchs-form" action="{{ route('matchgames.index') }}" method="GET" style="display: none;">
             @csrf
         </form>
-        <button class="btn btn-primary nav__item__button" onclick="event.preventDefault(); document.getElementById('matchs-form').submit();">Matchs</button>
+        <button class="nav__item__button" onclick="event.preventDefault(); document.getElementById('matchs-form').submit();">Matchs</button>
     </li>
     <li class="nav-item">
         <form id="zones-form" action="{{ route('zones.index') }}" method="GET" style="display: none;">
             @csrf
         </form>
-        <button class="btn btn-primary nav__item__button" onclick="event.preventDefault(); document.getElementById('zones-form').submit();">Zones</button>
+        <button class="nav__item__button" onclick="event.preventDefault(); document.getElementById('zones-form').submit();">Zones</button>
     </li>
     <li class="nav-item-logout">
         <form class="ml-auto" id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
-        <button class="btn btn-dark nav__item-logout__button" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</button>
+        <button class="nav__item-logout__button" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</button>
     </li>
 
 @endsection
--- resources/views/zones.blade.php
+++ resources/views/zones.blade.php
@@ -1,54 +1,54 @@
 @extends('home')
 @section('content')
 
-    <div id="test">
-        <div>Zones</div>
+    <div class="view__container">
+        <span class="view__container__title">Edit Zones</span>
         <hr />
-        <div class="table-responsive">
+        <div class="view__container__table-container">
             
-            <form method="GET" action="{{ route('zones.stadiumZones') }}">
+            <form method="GET" class="view__container__table-container__form-select" action="{{ route('zones.stadiumZones') }}">
                 @if($errors->any())
                 <h4>{{$errors->first()}}</h4>
                 @endif
                 <label for="stadiumId">Stadiums Available</label>
-                <select class="form-control" name="stadiumId">
+                <select class="view__container__table-container__form-select__select" name="stadiumId">
                     <option value="-1">Select Stadium</option>
                     @foreach ($stadiums as $stadium)
                     <option value="{{ $stadium->id }}">{{ $stadium->id }} - {{ $stadium->stadium_name }}</option>
                     @endforeach
                 </select>
-                <button class="btn btn-primary" type="submit" onclick="">Show Stadium Zones</button>
+                <button class="table-button select-button" type="submit" onclick="">Show Stadium Zones</button>
             </form>
-            <table class="table">
-                <thead>
+            <table class="view__container__table-container__table">
+                <thead class="view__container__table-container__table__head">
                 <tr>
                     <th scope="col">ID</th>
-                    <th scope="col">Created At</th>
-                    <th scope="col">Updated At</th>
                     <th scope="col">Price Addition</th>
                     <th scope="col">Stadium Location</th>
+                    <th scope="col">Created At</th>
+                    <th scope="col">Updated At</th>
                 </tr>
                 </thead>
-                <tbody>
+                <tbody class="view__container__table-container__table__body">
                 @foreach ($zones as $zone)
-                    <tr id="">
+                    <tr class="view__container__table-container__table__item">
                         <td>{{ $zone->id }}</td>
-                        <td>{{ $zone->created_at }}</td>
-                        <td>{{ $zone->updated_at }}</td>
                         <td>{{ $zone->price_addition }}</td>
                         <td>{{ $zone->stadium_location }}</td>
+                        <td>{{ $zone->created_at }}</td>
+                        <td>{{ $zone->updated_at }}</td>
                         <td>
                             <form method="GET" action="{{ route('zones.edit', $zone->id) }}">
                                     @csrf
                                     @method('GET')
-                                    <button class="btn btn-primary" type="submit" onclick="">Edit</button>
+                                    <button class="table-button" type="submit" onclick="">Edit</button>
                             </form>    
                         </td>
                     </tr>
                 @endforeach
                 </tbody>
             </table>
-            {{$zones->onEachSide(1)->links()}}
+            <div class="matchgame__pagination">{{$zones->onEachSide(1)->links()}}</div>
         </div>
     </div>
 @endsection
