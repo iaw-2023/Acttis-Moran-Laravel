2563271401fc40baa087ad6e7012f936b2216d6c
LeanM <leandro.moran.11@gmail.com>
2023-05-14 21:33:13 -0300

    Revert "Update 2023_03_22_193813_create_admins_table.php"
    This reverts commit 4f77266fd062d0a65e31545286c39ae0ecce7f37.

--- database/migrations/2023_03_22_193813_create_admins_table.php
+++ database/migrations/2023_03_22_193813_create_admins_table.php
@@ -14,11 +14,8 @@ return new class extends Migration
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
         });
     }
 
