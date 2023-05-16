478d49f04e4e04bf7ef11829dc43954207ab28a4
LeanM <leandro.moran.11@gmail.com>
2023-05-16 13:52:19 -0300

    Update email.blade.php

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,4 +1,5 @@
 @extends('app')
+@section('content')
     <div class="create-view__container">
         @if($errors->any())
             <div class="alert alert-danger alert-dismissible fade show" role="alert" style="position:fixed; top:5rem">
@@ -13,31 +14,30 @@
         </span>
         <div class="create-view__body">
         <form method="POST" action="{{ route('password.email') }}">
-    @csrf
+            @csrf
+            <div class="form-group row">
+                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electrónico') }}</label>
 
-    <div class="form-group row">
-        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electrónico') }}</label>
+                <div class="col-md-6">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-        <div class="col-md-6">
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
 
-            @error('email')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-        </div>
-    </div>
-
-    <div class="form-group row mb-0">
-        <div class="col-md-6 offset-md-4">
-            <button type="submit" class="btn btn-primary">
-                {{ __('Enviar enlace de restablecimiento de contraseña') }}
-            </button>
-        </div>
-    </div>
-</form>
+            <div class="form-group row mb-0">
+                <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Enviar enlace de restablecimiento de contraseña') }}
+                    </button>
+                </div>
+            </div>
+        </form>
             
         </div>
     </div>
-
+@endsection
