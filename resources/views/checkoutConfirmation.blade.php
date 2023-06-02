e007343d58e97c61b1f5637357b23efae99b84b0
LeanM <42581931+LeanM@users.noreply.github.com>
2023-06-01 21:13:52 -0300

    Update checkoutConfirmation.blade.php

--- resources/views/checkoutConfirmation.blade.php
+++ resources/views/checkoutConfirmation.blade.php
@@ -5,35 +5,42 @@
 </head>
 <body>
     <h1>Checkout Confirmation</h1>
-    <p>Thank you for your purchase!</p>
-    <p>Order ID: {{ $order->id }}</p>
+    <h4>Thank you for your purchase!</h4>
+    <p>Order ID Reference: {{ $order->id }}</p>
 
-    <h2>Tickets Purchased:</h2>
-    <ul>
+    <h3>Tickets Purchased:</h3>
+    
         @foreach ($ticketsDetails as $ticketDetail)
-            @php
-            $ticket = $ticketDetail->ticket;
-            $home_team = $ticket->matchgame->teamsPlayingMatch[0]->team->team_name;
-            $away_team = $ticket->matchgame->teamsPlayingMatch[1]->team->team_name;
-            $playing_on_date = $ticket->matchgame->played_on_date;
-            $playing_on_time = $ticket->matchgame->played_on_time;
-            $stadium_zone = $ticket->zone;
-            $stadium = $ticket->matchgame->stadium->stadium_name;
-            @endphp
-            <li>
-                 {{$home_team}} vs {{$away_team}} | {{$playing_on_date}} {{$playing_on_time}}
-            </li>
-            <li>
-                {{$stadium}} Zone: {{$stadium_zone->stadium_location}}
-            </li>
-            <li>
-                Price: {{$ticket->base_price + $stadium_zone->price_addition}}
-                Quantity: {{ $ticketDetail->ticket_quantity }}
-            </li>
-
+            <ul>
+                @php
+                $ticket = $ticketDetail->ticket;
+                $home_team = $ticket->matchgame->teamsPlayingMatch[0]->team->team_name;
+                $away_team = $ticket->matchgame->teamsPlayingMatch[1]->team->team_name;
+                $playing_on_date = $ticket->matchgame->played_on_date;
+                $playing_on_time = $ticket->matchgame->played_on_time;
+                $stadium_zone = $ticket->zone;
+                $stadium = $ticket->matchgame->stadium->stadium_name;
+                @endphp
+                <p>Ticket : </p>
+                <li>
+                     {{$home_team}} vs {{$away_team}}
+                </li>
+                <li>
+                     {{$playing_on_date}} {{$playing_on_time}}
+                </li>
+                <li>
+                    {{$stadium}} | {{$stadium_zone->stadium_location}}
+                </li>
+                <li>
+                    Ticket price: ${{$ticket->base_price + $stadium_zone->price_addition}}
+                </li>
+                <li>
+                    Ticket quantity: {{ $ticketDetail->ticket_quantity }}
+                </li>
+            </ul>
         @endforeach
-    </ul>
+    
 
-    <p>Total Price: {{ $totalPrice }}</p>
+    <p>Order Total Price: {{ $totalPrice }}</p>
 </body>
 </html>
