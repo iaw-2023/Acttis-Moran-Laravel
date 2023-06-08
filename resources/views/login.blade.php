c9b639c26c9742a8c18c8bc37031df45139dcef9
LeanM <42581931+LeanM@users.noreply.github.com>
2023-06-07 23:54:55 -0300

    Fix error on bad login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,12 +21,14 @@
 
                                     <div class="col-md-6">
                                         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                        @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                        @if($errors->any())
+                                        <div class="alert alert-danger alert-dismissible fade show" role="alert" style="position:fixed; top:5rem">
+                                            <strong>{{$errors->first()}}</strong>
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        @endif
                                     </div>
                                 </div>
 
@@ -35,12 +37,6 @@
 
                                     <div class="col-md-6">
                                         <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                        @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
                                     </div>
                                 </div>
 
